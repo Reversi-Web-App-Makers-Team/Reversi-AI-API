0eec500f0cd44136404a11b1e34cd92c86c3c621
Shion-0014 <seanmqe@gmail.com>
2019-03-24 11:48:06 +0900

    "bug fix"

--- dqn_v1.py
+++ dqn_v1.py
@@ -1,6 +1,8 @@
 import numpy as np
 from collections import namedtuple
 
+import os
+
 import random
 import torch
 from torch import nn
@@ -10,31 +12,37 @@ import torch.nn.functional as F
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 GAMMA = 0.99 # 時間割引率
-NUM_EPISODES = 1000 # 最大試行回数
+NUM_EPISODES = 1000000 # 最大試行回数
 BATCH_SIZE = 32
 CAPACITY = 10000 # メモリの容量
 
+NUM_ACTIONS = 64
+NUM_STATES = 65
 
-class ReplayMerory:
+class ReplayMemory:
     '''経験を保存するメモリクラスの定義'''
 
     def __init__(self, CAPACITY):
         self.capacity = CAPACITY # メモリの最大長さ
         self.memory = [] # 経験を保存する変数
-        self. inex = - # 保存するindexを示す変数
+        self. index = 0 # 保存するindexを示す変数
 
     def push(self, state, action, state_next, reward):
-        '''transition = (state, action, state_next, reward)をメモリに保存する'''
+        '''transition = (state, action, next_state, reward)をメモリに保存する'''
 
         if len(self.memory) < self.capacity:
             self.memory.append(None) # メモリが満タンでないときはたす
 
-            self.memory[self.index] = Transition(state, aciton, state_next, reward)
+            self.memory[self.index] = Transition(state, action, state_next, reward)
             self.index = (self.index + 1) % self.capacity # 保存するindexを一つずらす
 
     def sample(self, batch_size):
         '''batch_size分だけランダムに保存内容を取り出す'''
         return random.sample(self.memory, batch_size)
+    
+    def __len__(self):
+        '''関数lenに対して、現在の変数memoryの長さを返す'''
+        return len(self.memory)
 
 
 class Net(nn.Module):
@@ -61,16 +69,17 @@ class Brain:
         # NNを構築
         n_in, n_mid, n_out = num_states, 128, num_actions
         self.main_q_network = Net(n_in, n_mid, n_out)
-        self.target_q_network = net(n_in, n_mid, n_out)
+        self.target_q_network = Net(n_in, n_mid, n_out)
 
         # 最適化手法の設定
         self.optimizer = optim.Adam(self.main_q_network.parameters(), lr=0.0001)
 
-    def replay(self):
+    def replay(self, episode):
         '''Experience Replayでネットワークの結合パラメータを学習'''
 
         # 1.メモリサイズの確認
-        if len(self.memory) < BATCH_SIZE:
+        next_state = self.memory.memory[0].next_state
+        if len(self.memory) < BATCH_SIZE :
             return
 
         # 2.ミニバッチの作成
@@ -80,12 +89,15 @@ class Brain:
         self.expected_state_action_values = self.get_expected_state_action_values()
 
         # 4.結合パラメータの更新
-        self.update_main_q_network()
+        self.update_main_q_network(episode)
 
     def decide_action(self, state, episode):
         '''行動を決定する'''
-        epsilon = 0.5*(1/(episode+1))
-
+        if episode < 30000:
+            epsilon = 0.5
+        else:
+            epsilon = 0.5*(1/((episode-30000)+1))
+        
         if epsilon <= np.random.uniform(0, 1):
             self.main_q_network.eval() # 推論モード
 
@@ -94,25 +106,28 @@ class Brain:
                 # .view(1,1): [torch.LongTensor of size 1] → size 1x1
                 action = self.main_q_network(state).max(1)[1].view(1, 1)
         else:
-            # 行動をランダムに返す
-            action = torch.LongTensor([[random.randrange(self.num_actions)]])
+            # putable position の行動をランダムに返す
+            putable_pos, = np.where(state[0] == 2) # stateの先頭に手番が入っているので-1して盤面のindexにする
+            putable_pos += -1
+            action = torch.LongTensor([[random.choice(list(putable_pos))]])
 
         return action
 
     def make_minibatch(self):
         '''2.ミニバッチの作成'''
 
-        # メモリからミニバッチ分のデータを取り出す
+        # メモリからミニバッチ分のデータを取り出す（全てNoneのときは
         transitions = self.memory.sample(BATCH_SIZE)
-        
+
         # (state, action, state_next, reward) xBATCH → (state xBATCH, action xBATCH,..）
-        batch = Transition(*zip(*transitons))
+        batch = Transition(*zip(*transitions))
 
         # それぞれについて（例えばstateについて)
         # [torch.FloatTensor of size 1x65] xBATCH → torch.FloatTensor of size BATCH_SIZEx65
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
+        self.next_state_batch = torch.cat([s if s is not None else torch.Tensor([[np.nan for i in range(NUM_STATES)]]) for s in batch.next_state])
         non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
 
         return batch, state_batch, action_batch, reward_batch, non_final_next_states
@@ -130,7 +145,7 @@ class Brain:
 
         # 3.3 max{(Q(s_t+1, a)}を求める
         # next_stateがあるかをチェックするインデックスマスク
-        non_final_mask = torch.Bytetensor(tuple(map(lambda s:s is not None, self.batch.next_state)))
+        non_final_mask = torch.ByteTensor(tuple(map(lambda s:s is not None, self.batch.next_state)))
         # まずは全部0にしておく
         next_state_values = torch.zeros(BATCH_SIZE)
         a_m = torch.zeros(BATCH_SIZE).type(torch.LongTensor)
@@ -147,15 +162,15 @@ class Brain:
         # 3.4 教師となるQ(s_t, a_t)値を、Q学習の式から求める
 
         # ここで、現在の状態と次の状態の手番が同じか異なるかで場合分けが生じる
-        index_diff_value, = np.where(self.state_batch[non_final_mask,0] != torch.cat(self.batch.next_state)[non_final_mask,0])
+        index_diff_value, = np.where(self.state_batch[non_final_mask,0] != self.next_state_batch[non_final_mask,0])
         divided_frag = torch.ones(BATCH_SIZE)
         divided_frag[index_diff_value] = -1 * divided_frag[index_diff_value]
 
         expected_state_action_values = (self.reward_batch + GAMMA * next_state_values) * divided_frag
-        
+
         return expected_state_action_values
 
-    def update_main_q_netowrk(self):
+    def update_main_q_network(self, episode):
         '''4.　結合パラメータの更新'''
 
         # ネットワークを訓練モードに
@@ -163,9 +178,11 @@ class Brain:
 
         # 損失関数の計算
         loss = F.smooth_l1_loss(self.state_action_values, self.expected_state_action_values.unsqueeze(1))
-        
+        if episode % 1000 == 0:
+            print('loss : {}'.format(loss.item()))
+
         # パラメータの更新
-        self.optimaizer.zero_grad() # 勾配をリセット
+        self.optimizer.zero_grad() # 勾配をリセット
         loss.backward()
         self.optimizer.step()
 
@@ -177,11 +194,11 @@ class Brain:
 class Agent:
     def __init__(self, num_states, num_actions):
         '''課題の状態と行動の数を設定する'''
-        self.brain = Brain(num_states, num_actions) 
+        self.brain = Brain(num_states, num_actions)
 
-    def update_q_function(self):
+    def update_q_function(self, episode):
         '''Q関数を更新する'''
-        self.brain.replay()
+        self.brain.replay(episode)
 
     def get_action(self, state, episode):
         '''行動を決定する'''
@@ -194,23 +211,22 @@ class Agent:
 
     def update_target_q_function(self):
         ''' Target Q-NetworkをMain Q-Networkと同期'''
-        self.brain.update_q_network()
+        self.brain.update_target_q_network()
 
 class Environment:
 
     def __init__(self):
-        num_states = 65
-        num_actions = 64
+        num_states = NUM_STATES
+        num_actions = NUM_ACTIONS
         self.agent = Agent(num_states, num_actions)
 
     def run(self):
 
         for episode in range(NUM_EPISODES):
-            state, player = self.env.reset() # 環境の初期化
+            state, player = reset() # 環境の初期化
 
             state = np.array(state)
-            player = np.array([player])
-            state = np.append(player, state)
+            state = np.append(np.array([player]), state)
 
             state = torch.from_numpy(state).type(torch.FloatTensor) # numpy　→ torch.FloatTensor
             state = torch.unsqueeze(state, 0) # size65 → size 1x65
@@ -221,11 +237,7 @@ class Environment:
 
                 # 行動a_tの実行により、s_{t+1}とdoneフラグを求める
                 state_raw = list(np.array(state[0][1:]))
-                state_next, player, win_los_frag = self.env.step(state_raw, action.item(), player)
-                
-                state_next = np.array(state_next)
-                player = np.array([player])
-                state_next = np.append(player, state_next)
+                state_next, player, win_los_frag = step(state_raw, action.item(), player)
 
                 # 終了したとき（勝ち負けが決まったか、反則を犯したとき）
                 if win_los_frag == 1:
@@ -233,20 +245,22 @@ class Environment:
 
                 elif win_los_frag == -1:
                     reward = torch.FloatTensor([-1.0]) # 負けたとき
-                
+
                 elif win_los_frag == 2:
                     reward = torch.FloatTensor([0.0]) # 引き分けのとき
 
                 else:
                     reward = torch.FloatTensor([0.0]) # 普段は報酬0
+                    state_next = np.array(state_next)
+                    state_next = np.append(np.array([player]), state_next)
                     state_next = torch.from_numpy(state_next).type(torch.FloatTensor)
-                    state_next = torch.unsqueeze(state, 0)
+                    state_next = torch.unsqueeze(state_next, 0)
 
                 # メモリに経験を追加
                 self.agent.memorize(state, action, state_next, reward)
 
                 # Experience ReplayでQ関数を更新する
-                self.agent.update_q_function()
+                self.agent.update_q_function(episode)
 
                 # 観測の更新
                 state = state_next
@@ -257,5 +271,10 @@ class Environment:
                         self.agent.update_target_q_function()
                     break
 
+        
+        #resultsディレクトリを作成
+        result_dir = 'results'
+        if not os.path.exists(result_dir):
+            os.mkdir(result_dir)           
         # 終了時モデル保存
-        torch.save(self.agent.brain.main_q_network.state_dict(), 'reversiWebAPP/reversiApp/models/model1.pt')
+        torch.save(self.agent.brain.main_q_network.state_dict(), '/content/results/model2.pt')
